1196066bcf7b514f04ba73af8494b8cdd28d6a8b
Dénice <delton.denice@gmail.com>
2022-11-30 16:48:28 +0100

    Added DrivingLicenceFincerServiceTest

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
@@ -1,11 +1,18 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DrivingLicenceFinderServiceTest {
@@ -18,11 +25,17 @@ class DrivingLicenceFinderServiceTest {
 
     @Test
     void should_find() {
-
+        UUID drivingLicenceUUID = UUID.fromString("eef93bc4-70c1-11ed-a1eb-0242ac120002");
+        when(database.findById(drivingLicenceUUID)).thenReturn(Optional.ofNullable(DrivingLicence.builder().id(drivingLicenceUUID).availablePoints(12).build()));
+        Optional driverLicence = service.findById(drivingLicenceUUID);
+        assertThat(driverLicence.isPresent()).isTrue();
     }
 
     @Test
     void should_not_find() {
-
+        UUID drivingLicenceUUID = UUID.fromString("eef93bc4-70c1-11ed-a1eb-0242ac120002");
+        when(database.findById(drivingLicenceUUID)).thenReturn(Optional.empty());
+        Optional<DrivingLicence> driverLicence = service.findById(drivingLicenceUUID);
+        assertThat(driverLicence.isEmpty()).isTrue();
     }
 }
