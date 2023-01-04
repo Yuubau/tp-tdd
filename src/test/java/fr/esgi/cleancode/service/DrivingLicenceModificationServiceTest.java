89bff1d161fd5a7eba2a410772957570cf82cf45
Dénice <delton.denice@gmail.com>
2023-01-04 09:03:08 +0100

    DrivingLicenceModificationService Tests

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceModificationServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceModificationServiceTest.java
@@ -29,7 +29,7 @@ class DrivingLicenceModificationServiceTest {
     @Test
     void remove_point_existing_driving_licence() {
         UUID drivingLicenceUUID = UUID.fromString("eef93bc4-70c1-11ed-a1eb-0242ac120002");
-        when(drivingLicenceFinderService.findById(drivingLicenceUUID)).thenReturn(Optional.ofNullable(DrivingLicence.builder().id(drivingLicenceUUID).availablePoints(12).build()));
+        when(drivingLicenceFinderService.findById(drivingLicenceUUID)).thenReturn(Optional.ofNullable(DrivingLicence.builder().id(drivingLicenceUUID).availablePoints(12).driverSocialSecurityNumber("152435243524345").build()));
 
         DrivingLicence drivingLicence = service.removeDrivingLicencePoint(drivingLicenceUUID, 12);
 
