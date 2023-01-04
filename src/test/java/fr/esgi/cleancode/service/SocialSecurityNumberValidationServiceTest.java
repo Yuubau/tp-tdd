f9eb7a1e3ea3bd9bfc2c682e6121584054d0bbdd
Dénice <delton.denice@gmail.com>
2023-01-04 09:03:33 +0100

    SocialSecurityNumberValidationService Tests

--- src/test/java/fr/esgi/cleancode/service/SocialSecurityNumberValidationServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/SocialSecurityNumberValidationServiceTest.java
@@ -1,42 +1,32 @@
 package fr.esgi.cleancode.service;
-
-import fr.esgi.cleancode.database.InMemoryDatabase;
-import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class SocialSecurityNumberValidationServiceTest {
 
-    @Mock
-    private SocialSecurityNumberValidationService service;
+    private SocialSecurityNumberValidationService service = new SocialSecurityNumberValidationService();
 
     @Test
-    void valid_security_number_should_verify() {
+    void valid_security_number_should_be_true() {
         assertThat(service.verifySecurityNumberValidity("093847362543253")).isTrue();
     }
 
     @Test
-    void null_security_number_should_fail() {
-        assertThatExceptionOfType(InvalidDriverSocialSecurityNumberException.class).isThrownBy(() -> {
-            service.verifySecurityNumberValidity(null);
-        });
+    void null_security_number_should_be_false() {
+        assertThat(service.verifySecurityNumberValidity(null)).isFalse();
     }
 
     @Test
-    void alpha_numerical_security_number_should_fail() {
-        assertThatExceptionOfType(InvalidDriverSocialSecurityNumberException.class).isThrownBy(() -> {
-            service.verifySecurityNumberValidity("ahd635476354hfudh");
-        });
+    void alpha_numerical_security_number_should_be_false() {
+        assertThat(service.verifySecurityNumberValidity("ahd635476354hfudh")).isFalse();
+
     }
 
     @Test
-    void security_number_size_13_should_fail() {
-        assertThatExceptionOfType(InvalidDriverSocialSecurityNumberException.class).isThrownBy(() -> {
-            service.verifySecurityNumberValidity("2435463726354");
-        });
+    void security_number_size_13_should_be_false() {
+        assertThat(service.verifySecurityNumberValidity("2435463726354")).isFalse();
+
     }
 
 }
